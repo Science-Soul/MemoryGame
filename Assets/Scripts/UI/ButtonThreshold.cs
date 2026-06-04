c6594c3e344b7b7b9966bd3780f1809f55e11e48
Богдан Волошин <voloshinbogdann@gmail.com>
2026-06-04 17:34:30 +0700

    Нарисовал кнопки, расположил на меню города. Сделал анимацию. Нужно оптимизировать вес.

---
+++ Assets/Scripts/UI/ButtonThreshold.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonThreshold : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gameObject.GetComponent<Image>().alphaHitTestMinimumThreshold = 0.1f;
+    }
+}
