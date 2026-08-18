0174c022eb81a5c77723fd1021cf6413d72b4f78
Bogdan V. <voloshinbogdann@gmail.com>
2026-08-18 20:45:04 +0700

    Добавил кнопку выхода в меню.

---
+++ Assets/Scripts/UI/ExitButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.AddressableAssets;
+
+public class ExitButton : MonoBehaviour
+{
+    public AssetReference sceneReference;
+
+    public void BackToMainMenu()
+    {
+        Addressables.LoadSceneAsync(sceneReference, LoadSceneMode.Single);
+    }
+}
